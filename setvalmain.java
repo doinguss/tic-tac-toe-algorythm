cf245596251a5dc5de8b578e8b8df33408057dc6
april <doingusmcscrumply@gmail.com>
2026-03-08 15:22:01 +0100

    fixed rotations
    some odd stuff with the .clone() method so switched so it used the param as a ref isnstead, knocked out ca 20 board states down to 111

--- setvalmain.java
+++ setvalmain.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 public class setvalmain {
 	private ArrayList<String> sets;
-	public setvalmain() {
+	private setvalmain() {
 		sets=new ArrayList<String>();
 		validate(
 				new String[][]{{".",".","."},{".",".","."},{".",".","."}},
 				new String[][]{{".",".","."},{".","O","."},{".",".","."}},
 				new String[][]{{".","O","."},{".","X","O"},{".",".","."}},
-				new String[][]{{".","O","."},{".","X","."},{".","O","."}},
+				new String[][]{{".","O","."},{".","X","."},{".","O","."}},////0->1
 				new String[][]{{"X",".","."},{".","O","."},{".",".","."}},
 				new String[][]{{"X",".","X"},{".","X","O"},{"O",".","O"}},
 				new String[][]{{"X",".","O"},{"O","X","X"},{"X","O","O"}},
@@ -17,7 +17,7 @@ public class setvalmain {
 				new String[][]{{"X",".","O"},{"O","O","X"},{"X",".","O"}},
 				new String[][]{{"X",".","X"},{"X","O","O"},{"O","O","."}},
 				new String[][]{{"X",".","O"},{"O","O","X"},{"X","O","."}},
-				new String[][]{{"X",".","X"},{"X","O","O"},{"O",".","O"}},
+				new String[][]{{"X",".","X"},{"X","O","O"},{"O",".","O"}},////1->2
 				new String[][]{{"X","X","."},{".","O","O"},{".",".","."}},
 				new String[][]{{"X","X","."},{".","O","."},{".",".","O"}},
 				new String[][]{{"X","X","."},{".","O","."},{".","O","."}},
@@ -29,17 +29,15 @@ public class setvalmain {
 				new String[][]{{"O","O","."},{".","X","O"},{"X",".","."}},
 				new String[][]{{"O",".","."},{"O","X","O"},{"X",".","."}},
 				new String[][]{{"O",".","."},{".","X","O"},{"X","O","."}},
-				new String[][]{{"O",".","."},{".","X","O"},{"X",".","O"}},
-				new String[][]{{"O","O","."},{".","X","."},{"X",".","O"}},
-				new String[][]{{"O",".","."},{"O","X","."},{"X",".","O"}},//* 27*/ new String[][]{{"O",".","."},{".","X","O"},{"X",".","O"}},
-				new String[][]{{"O",".","."},{".","X","."},{"X","O","O"}},
+				new String[][]{{"O",".","."},{".","X","O"},{"X",".","O"}},		//new String[][]{{"O","O","."},{".","X","."},{"X",".","O"}},
+				new String[][]{{"O",".","."},{"O","X","."},{"X",".","O"}},			//* 27*/ new String[][]{{"O",".","."},{".","X","O"},{"X",".","O"}},		//new String[][]{{"O",".","."},{".","X","."},{"X","O","O"}},
 				new String[][]{{"X",".","."},{"X","O","O"},{"O",".","."}},
 				new String[][]{{"X","O","."},{".","X","O"},{".",".","O"}},
 				new String[][]{{"X","O","."},{"O","O","X"},{"O","X","."}},
 				new String[][]{{"X","O","."},{"O","O","X"},{".","X","O"}},
 				new String[][]{{"X","O","."},{"X","O","O"},{"O","X","."}},
 				new String[][]{{"X","O","."},{"O","X","."},{"X","O","O"}},
-				new String[][]{{"X","O","."},{".","X","O"},{"X","O","O"}},
+				new String[][]{{"X","O","."},{".","X","O"},{"X","O","O"}},////2->3
 				new String[][]{{"X","O","O"},{".","X","X"},{".",".","O"}},
 				new String[][]{{"X",".","O"},{".","X","X"},{"O",".","O"}},
 				new String[][]{{"X",".","O"},{".","X","X"},{".","O","O"}},
@@ -54,18 +52,14 @@ public class setvalmain {
 				new String[][]{{"X",".","O"},{".","O","."},{"X",".","O"}},
 				new String[][]{{"O","O","X"},{".","X","."},{"O",".","."}},
 				new String[][]{{"O","X","O"},{".","X","."},{".","O","."}},
-				new String[][]{{"X","O","O"},{".","O","O"},{"X","X","."}},
-				new String[][]{{"X","O","O"},{".","O","."},{"X","X","O"}},
-				new String[][]{{"X","O","X"},{".","O","O"},{"O","X","."}},
-				new String[][]{{"X","O","X"},{".","O","."},{"O","X","O"}},//* 54*/ new String[][]{{"X","O","O"},{".","O","."},{"X","X","O"}},
-				new String[][]{{"X","O","."},{".","O","O"},{"X","X","O"}},
-				new String[][]{{"X","O","O"},{".","X","."},{"X","O","O"}},
+				new String[][]{{"X","O","O"},{".","O","O"},{"X","X","."}},		//new String[][]{{"X","O","O"},{".","O","."},{"X","X","O"}},		//new String[][]{{"X","O","X"},{".","O","O"},{"O","X","."}},		//new String[][]{{"X","O","X"},{".","O","."},{"O","X","O"}},			//* 54*/ new String[][]{{"X","O","O"},{".","O","."},{"X","X","O"}},		//new String[][]{{"X","O","."},{".","O","O"},{"X","X","O"}},
+				new String[][]{{"X","O","O"},{".","X","."},{"X","O","O"}},////3->4
 				new String[][]{{"X","O","."},{".",".","."},{".",".","."}},
 				new String[][]{{"X",".","O"},{".",".","."},{".",".","."}},
 				new String[][]{{"X",".","."},{".",".","O"},{".",".","."}},
 				new String[][]{{"X",".","."},{".",".","."},{".",".","O"}},
 				new String[][]{{"O",".","."},{".",".","."},{".",".","."}},
-				new String[][]{{".","O","."},{".",".","."},{".",".","."}},
+				new String[][]{{".","O","."},{".",".","."},{".",".","."}},////4->5
 				new String[][]{{"X",".","O"},{".","X","."},{".",".","O"}},
 				new String[][]{{"X","X","O"},{"O","O","."},{"X",".","."}},
 				new String[][]{{"X","O","."},{"O","O","."},{".","X","."}},
@@ -75,7 +69,7 @@ public class setvalmain {
 				new String[][]{{"O","X","O"},{".","X","."},{"X","O","O"}},
 				new String[][]{{"O","O","X"},{"X","X","."},{"O","O","."}},
 				new String[][]{{"O","O","X"},{"X","X","."},{"O",".","O"}},
-				new String[][]{{"O","X","O"},{"X","X","."},{"O","O","."}},
+				new String[][]{{"O","X","O"},{"X","X","."},{"O","O","."}},////5->6
 				new String[][]{{"O","X","O"},{"X","X","."},{".","O","O"}},
 				new String[][]{{"X","O","."},{".","X","."},{".",".","O"}},
 				new String[][]{{"X","X","O"},{".","O","."},{".",".","."}},
@@ -94,27 +88,18 @@ public class setvalmain {
 				new String[][]{{"X",".","."},{"X","O","O"},{".","O","."}},
 				new String[][]{{"X",".","."},{"X","O","O"},{".",".","O"}},
 				new String[][]{{"O","O","X"},{"O","X","."},{".",".","."}},
-				new String[][]{{"O","O","X"},{".","X","O"},{".",".","."}},
-				new String[][]{{"O","O","X"},{".","X","."},{".","O","."}},
-				new String[][]{{"O","O","X"},{".","X","."},{".",".","O"}},
-				new String[][]{{"X","O","."},{".","X","."},{".","O","O"}},
-				new String[][]{{"X","O","O"},{"O","O","X"},{".","X","."}},
+				new String[][]{{"O","O","X"},{".","X","O"},{".",".","."}},		//new String[][]{{"O","O","X"},{".","X","."},{".","O","."}},		//new String[][]{{"O","O","X"},{".","X","."},{".",".","O"}},
+				new String[][]{{"X","O","."},{".","X","."},{".","O","O"}},		//new String[][]{{"X","O","O"},{"O","O","X"},{".","X","."}},
 				new String[][]{{"X","O","O"},{"X","O","O"},{".","X","."}},
-				new String[][]{{"X","O","."},{"X","O","O"},{".","X","O"}},
-				new String[][]{{"X","O","X"},{"O","X","O"},{".",".","O"}},//* 99*/ new String[][]{{"X","O","X"},{"O","X","O"},{".",".","O"}},
-				new String[][]{{"X","O","X"},{".","X","O"},{".","O","O"}},
-				new String[][]{{"X","O","O"},{"O","X","X"},{"X",".","O"}},
+				new String[][]{{"X","O","."},{"X","O","O"},{".","X","O"}},		//new String[][]{{"X","O","X"},{"O","X","O"},{".",".","O"}},			//* 99*/ new String[][]{{"X","O","X"},{"O","X","O"},{".",".","O"}},
+				new String[][]{{"X","O","X"},{".","X","O"},{".","O","O"}},////6->7		//new String[][]{{"X","O","O"},{"O","X","X"},{"X",".","O"}},
 				new String[][]{{"X","X","O"},{"O","O","X"},{"X",".","O"}},
 				new String[][]{{"X","O","."},{".","O","."},{".",".","."}},
-				new String[][]{{"O","X","O"},{"O","X","."},{".",".","."}},//* 105*/ new String[][]{{"O","X","O"},{".","X","O"},{".",".","."}},
-				new String[][]{{"O","X","O"},{".","X","."},{"O",".","."}},//* 107*/ new String[][]{{"O","X","O"},{".","X","."},{".",".","O"}},
-				new String[][]{{"X","O","O"},{"O","O","X"},{"X",".","."}},
-				new String[][]{{"O","X","O"},{"O","X","O"},{"X",".","."}},
-				new String[][]{{"O","X","O"},{".","X","O"},{"X",".","O"}},
-				new String[][]{{"O","X","O"},{"O","X","."},{"X",".","O"}},
-				new String[][]{{"X","O","X"},{"X","O","O"},{"O",".","."}},
+				new String[][]{{"O","X","O"},{"O","X","."},{".",".","."}},			//* 105*/ new String[][]{{"O","X","O"},{".","X","O"},{".",".","."}},
+				new String[][]{{"O","X","O"},{".","X","."},{"O",".","."}},			//* 107*/ new String[][]{{"O","X","O"},{".","X","."},{".",".","O"}},		//new String[][]{{"X","O","O"},{"O","O","X"},{"X",".","."}},		//new String[][]{{"O","X","O"},{"O","X","O"},{"X",".","."}},
+				new String[][]{{"O","X","O"},{".","X","O"},{"X",".","O"}},		//new String[][]{{"O","X","O"},{"O","X","."},{"X",".","O"}},		//new String[][]{{"X","O","X"},{"X","O","O"},{"O",".","."}},
 				new String[][]{{"O","O","X"},{"X","X","O"},{"O",".","."}},
-				new String[][]{{"X","O","X"},{".","X","O"},{"O",".","O"}},
+				new String[][]{{"X","O","X"},{".","X","O"},{"O",".","O"}},////7->8
 				new String[][]{{"X","O","O"},{".","X","."},{".",".","."}},
 				new String[][]{{"X","O","."},{".","X","O"},{".",".","."}},
 				new String[][]{{"X","O","."},{".","X","."},{".","O","."}},
@@ -125,11 +110,9 @@ public class setvalmain {
 				new String[][]{{"X",".","O"},{".","X","."},{"O",".","."}},
 				new String[][]{{"X",".","."},{".","X","O"},{".","O","."}},
 				new String[][]{{"X","X","O"},{"O","O","X"},{"X","O","."}},
-				new String[][]{{"O",".","O"},{".","X","O"},{"X",".","."}},//* 126*/ new String[][]{{"X","O","O"},{".","X","O"},{".",".","."}},
-				new String[][]{{"X","O","."},{"O","X","O"},{".",".","."}},
-				new String[][]{{"X","O","."},{".","X","O"},{"O",".","."}},
-				new String[][]{{"X","O","."},{".","X","O"},{".","O","."}},//* 130*/ new String[][]{{"X","O","O"},{".","X","."},{".","O","."}},
-				new String[][]{{"X","O","."},{"O","X","."},{".","O","."}},//* 132*/ new String[][]{{"X","O","."},{".","X","O"},{".","O","."}},
+				new String[][]{{"O",".","O"},{".","X","O"},{"X",".","."}},			//* 126*/ new String[][]{{"X","O","O"},{".","X","O"},{".",".","."}},
+				new String[][]{{"X","O","."},{"O","X","O"},{".",".","."}},		//new String[][]{{"X","O","."},{".","X","O"},{"O",".","."}},
+				new String[][]{{"X","O","."},{".","X","O"},{".","O","."}},			//* 130*/ new String[][]{{"X","O","O"},{".","X","."},{".","O","."}},		//new String[][]{{"X","O","."},{"O","X","."},{".","O","."}},			//* 132*/ new String[][]{{"X","O","."},{".","X","O"},{".","O","."}},
 				new String[][]{{"X","O","."},{".","X","."},{"O","O","."}},
 				new String[][]{{"X","O","O"},{"O","O","."},{"X","X","."}},
 				new String[][]{{"O","X","O"},{".","X","O"},{"X","O","."}},
@@ -143,8 +126,8 @@ public class setvalmain {
 	}
 
 	//from algorytm.tictactoe2.js
-	private String[][] trsp(String[][] b) {String[][] o=b.clone();String temp;for(int i=0;i<3;i++){for(int j=0;j<3;j++){   	temp=o[i][j];o[i][j]=o[j][i];o[j][i]=temp;}}return o;}//mirrors on diagonal
-	private String[][] rev (String[][] b) {String[][] o=new String[3][3];for(int i=0;i<3;i++){for(int j=0;j<3;j++){			o[i][j]=b[i][2-j]         ;}}return o;}//reverses the order in each row
+	private String[][] trsp(String[][] b) {String[][] o=new String[3][3];for(int i=0;i<3;i++){for(int j=0;j<3;j++){ o[i][j]=b[j][i]			  ;}}return o;}//mirrors on diagonal
+	private String[][] rev (String[][] b) {String[][] o=new String[3][3];for(int i=0;i<3;i++){for(int j=0;j<3;j++){	o[i][j]=b[i][2-j]         ;}}return o;}//reverses the order in each row
 
 	//w111 10g 411 1n574nc35 0f r3p3471ng b04rd 574735 (wr173n 1n 13375p34k 4 funn5i35)
 	private void validate(String[][]... mtrx){
@@ -174,7 +157,41 @@ public class setvalmain {
 	private String cont(String[][] s) {
 		return "|"+s[0][0]+s[0][1]+s[0][2]+"|\n\t\t\t|"+s[1][0]+s[1][1]+s[1][2]+"|\n\t\t\t|"+s[2][0]+s[2][1]+s[2][2]+"|";
 	}
-	public void nill() {}
+	private void nill() {
+		/* add a "*" here-->/
+		String[][] test= {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		/** /
+		test=trsp(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=trsp(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		test=rev(test);
+		System.out.println("\n\t\t\t"+cont(test));
+		/**/
+	}
 
 
 }
