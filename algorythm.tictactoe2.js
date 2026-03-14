d263934890605059ec355e5d4901b9133764ea7f
april <doingusmcscrumply@gmail.com>
2026-03-14 20:23:45 +0100

    temp display
    added some functions and stuff to the algorythm and html page to display and handle game states ect ect

--- algorythm.tictactoe2.js
+++ algorythm.tictactoe2.js
@@ -5,7 +5,7 @@ class mything{
     }
     trsp(b) {let o=[["","",""],["","",""],["","",""]];for(let i=0;i<3;i++){for(let j=0;j<3;j++){                     o[i][j]=b[j][i];                       }}return o;}//mirrors on diagonal/ transposes the matrix
     rev (b) {let o=[["","",""],["","",""],["","",""]];for(let i=0;i<3;i++){for(let j=0;j<3;j++){                    o[i][j]=b[i][2-j];                      }}return o;}//reverses the order in each row
-    mx2s(b) {let o=""                                ;for(let i=0;i<3;i++){for(let j=0;j<3;j++){if(b[i][j]===""){o+='.';}else{o+=(b[i][j]==this.p?'X':'O');}}}return o;}//generates a 1d string from 2d matrix(3X3) replaces empty with '.'
+    mx2s(b) {let o=""                                ;for(let i=0;i<3;i++){for(let j=0;j<3;j++){if(b[i][j]===""){o+='.';}else{o+=(b[i][j]==this.p?'x':'o');}}}return o;}//generates a 1d string from 2d matrix(3x3) replaces empty with '.'
     turn(b) {//the thing getting called at each turn, checks if boardstate is in lookuptable otherwise manipulates the matrix and looks again untill it finds a match
         let o=structuredClone(b),u=[[0,1,2],[3,4,5],[6,7,8]];// u= index matrix, is manipulated the same way the data matrix is but retains the original position of each position in form of value
         let i=this.lookup(o),err=15;
@@ -20,7 +20,6 @@ class mything{
             if(--err===0){alert("error");return;}
         }
         b[Math.floor(u[Math.floor(i*0.3625)][i%3]*0.3625)][u[Math.floor(i*0.3625)][i%3]%3]=this.p; //the const is 2.9/8 and im taking the floor of that so that itll increase by 1 every third exluding the final one so its 0 0 0 1 1 1 2 2 2 for all the numbers 0 trhough 8
-        //console.log(this.mx2s(b));//debug
     }
     lookup(o) {//lookup table for compressed gamestates, returns optimal* move (*i dont know its optimal its the best i can do tho xd)
         let table={
@@ -38,22 +37,16 @@ class mything{
             "xox.xo.oo": 6, "xoo.o..x.": 6, "xooxoo.x.": 6, "xo.xoo.xo": 6, "xooxo..xo": 6, "x.o.o....": 6, "xxooxo...": 7, "xxoox.o..": 7, 
             "xxoooxx.o": 7, "oxoox....": 7, "oxo.x.o..": 7, "ox.oxo...": 7, "ox..xoo..": 7, "ox..xo..o": 7, "ox.ox...o": 7, "ox..x.o.o": 7, 
             "xox.xoo.o": 7, "xo..o....": 7, "xo.xxoo..": 8, "x.oxxoo..": 8, "x.oxx.oo.": 8, "xxoooxxo.": 8, "oxoxxo.o.": 8, "oxx.xooo.": 8, 
-            "xo.oxo...": 8, "xo..xoo..": 8, "xo..xo.o.": 8, "xoooo.xx.": 8 
-            /*
-            ".........": 0,	"....O....": 0,	".O..XO...": 0,	".O..X..O.": 0,	"X...O....": 1,	"X.X.XOO.O": 1,	"X.OOXXXOO": 1,	"O.O.X....": 1,
-			"O.O.X.X.O": 1,	"X.OOOXX.O": 1,	"X.XXOOOO.": 1,	"X.OOOXXO.": 1,	"X.XXOOO.O": 1,	"XX..OO...": 2,	"XX..O...O": 2,	"XX..O..O.": 2,
-			"XX..O.O..": 2,	"XX.OO....": 2,	"X...XO..O": 2,	"OO..X....": 2,	"XO..O.OX.": 2,	"OO..XOX..": 2,	"O..OXOX..": 2,	"O...XOXO.": 2,
-			"O...XOX.O": 2,	"O..OX.X.O": 2,	"X..XOOO..": 2,	"XO..XO..O": 2,	"XO.OOXOX.": 2,	"XO.OOX.XO": 2,	"XO.XOOOX.": 2,	"XO.OX.XOO": 2,
-			"XO..XOXOO": 2,	"XOO.XX..O": 3,	"X.O.XXO.O": 3,	"X.O.XX.OO": 3,	"XXO.OOX..": 3,	"XXO.O.XO.": 3,	"XXO.O.X.O": 3,	"X...OO...": 3,
-			"XO..OO.X.": 3,	"XOO.O.X..": 3,	"X.O.OOX..": 3,	"X.O.O.XO.": 3,	"X.O.O.X.O": 3,	"OOX.X.O..": 3,	"OXO.X..O.": 3,	"XOO.OOXX.": 3,
-			"XOO.X.XOO": 3,	"XO.......": 4,	"X.O......": 4,	"X....O...": 4,	"X.......O": 4,	"O........": 4,	".O.......": 4,	"X.O.X...O": 5,
-			"XXOOO.X..": 5,	"XO.OO..X.": 5,	"X.OOO.X..": 5,	"XOXOO.OX.": 5,	"XO.OO.XXO": 5,	"OXO.X.XOO": 5,	"OOXXX.OO.": 5,	"OOXXX.O.O": 5,
-			"OXOXX.OO.": 5,	"OXOXX..OO": 6,	"XO..X...O": 6,	"XXO.O....": 6,	"XOX.XO..O": 6,	"X.X.XO.OO": 6,	"X.XOXO..O": 6,	"X.OOXX..O": 6,
-			"X.O.O....": 6,	"X...O...O": 6,	"O...XO...": 6,	"O...X...O": 6,	"XOO.O..X.": 6,	"XO..O..XO": 6,	"XO.XOO...": 6,	"X.OXOO...": 6,
-			"X..XOO.O.": 6,	"X..XOO..O": 6,	"OOXOX....": 6,	"OOX.XO...": 6,	"XO..X..OO": 6,	"XOOXOO.X.": 6,	"XO.XOO.XO": 6,	"XOX.XO.OO": 6,
-			"XXOOOXX.O": 7,	"XO..O....": 7,	"OXOOX....": 7,	"OXO.X.O..": 7,	"OXO.XOX.O": 7,	"OOXXXOO..": 7,	"XOX.XOO.O": 7,	"XOO.X....": 8,
-			"XO..XO...": 8,	"XO..X..O.": 8,	"XO..X.O..": 8,	"XO.OX....": 8,	"X.O.XO...": 8,	"X.O.X..O.": 8,	"X.O.X.O..": 8,	"X...XO.O.": 8,
-			"XXOOOXXO.": 8,	"O.O.XOX..": 8,	"XO.OXO...": 8,	"XO..XO.O.": 8,	"XO..X.OO.": 8,	"XOOOO.XX.": 8,	"OXO.XOXO.": 8,	"OXOXXO.O.": 8, "XO.OX.X.O" :2 */}
+            "xo.oxo...": 8, "xo..xoo..": 8, "xo..xo.o.": 8, "xoooo.xx.": 8 }
         return table[this.mx2s(o)]==null?   -1:table[this.mx2s(o)];
+        }
+        disp(b) {let o="=====\n";
+            for(let i=0;i<3;i++){
+                o+="|";
+                for(let j=0;j<3;j++){
+                    if(b[i][j]===""){o+='.';}
+                    else{o+=(b[i][j]==this.p?this.p:this.q);}}
+                o+="|\n";}
+            o+="=====\n";
+        return o;}
     }
-}
